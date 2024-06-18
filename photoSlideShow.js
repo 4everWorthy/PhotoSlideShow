84d17f24f47e391cc69c38df88b6d85aa4f67acf
Jason Worthy <109769584+4everWorthy@users.noreply.github.com>
2024-06-18 12:36:53 -0400

    1st commit

---
+++ photoSlideShow.js
@@ -0,0 +1,30 @@
+const photoSlideShow = { 
+    photoList: ["photo1.jpg", "photo2,jpg", "photo3.jpg"],
+    currentPhotoIndex: 0, 
+    nextPhoto() {   
+        if (this.currentPhotoIndex < this.photoList.length - 1) {
+            this.currentPhotoIndex++;
+            console.log(this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log("End of slideshow");
+        }
+    },
+
+    prevPhoto() {
+        if (this.currentPhotoIndex > 0) {
+            this.currentPhotoIndex--;
+            console.log(this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log("Beginning of slideshow");
+        }
+    },
+
+    getCurrentPhoto() {
+        return this.photoList[this.currentPhotoIndex];
+    },
+        
+};
+
+photoSlideShow.nextPhoto();
+photoSlideShow.nextPhoto();
+photoSlideShow.nextPhoto();
